1e659f99e505ebe64e45593a9ec7c9bfd215fdec
Arathisreenivasan <arathisreenivasan421@gmail.com>
2022-12-18 18:04:49 +0530

    added

---
+++ ecommerceproject/shopapp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Category,Product
+# Register your models here.
+class CAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(Category,CAdmin)
+
+class PAdmin(admin.ModelAdmin):
+    list_display = ['name','price','stock','available','created','updated']
+    list_editable = ['price','stock','available']
+    prepopulated_fields = {'slug':('name',)}
+    list_per_page = 20
+admin.site.register(Product,PAdmin)
